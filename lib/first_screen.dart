3bb8c65db614b849331f6a5eeb76b73ed311dfb6
amirahmadadibi <amirahmadadibi@gmail.com>
2023-05-22 23:34:01 +0330

    S09 E23 fix colors and stylesc

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -23,84 +23,93 @@ class _FirstScreenState extends State<FirstScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Color(0xff13005A),
         body: ListView.builder(
-      itemCount: cryptoList.length,
-      itemBuilder: (context, index) {
-        return Container(
-          height: 100,
-          margin: EdgeInsets.only(bottom: 10),
-          child: Row(
-            children: [
-              SizedBox(
-                width: 8,
-              ),
-              Text(
-                '${cryptoList[index].rank}',
-                style: TextStyle(fontSize: 20),
-              ),
-              SizedBox(
-                width: 8,
-              ),
-              Container(
-                height: 40,
-                width: 40,
-                child: CachedNetworkImage(
-                  imageUrl:
-                      'https://assets.coincap.io/assets/icons/${cryptoList[index].symbol.toLowerCase()}@2x.png',
-                  placeholder: (context, url) {
-                    return Container(
-                      height: 40,
-                      width: 40,
-                      decoration: BoxDecoration(
-                          color: Colors.grey[200],
-                          borderRadius: BorderRadius.circular(12)),
-                    );
-                  },
-                ),
-              ),
-              SizedBox(
-                width: 8,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
+          itemCount: cryptoList.length,
+          itemBuilder: (context, index) {
+            return Container(
+              height: 100,
+              margin: EdgeInsets.only(bottom: 10),
+              color: Color(0xff00337C),
+              child: Row(
                 children: [
-                  Text(cryptoList[index].name, style: TextStyle(fontSize: 20)),
-                  Text(cryptoList[index].symbol,
-                      style: TextStyle(fontSize: 20)),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    '${cryptoList[index].rank}',
+                    style: TextStyle(fontSize: 24, color: Colors.white),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Container(
+                    height: 45,
+                    width: 45,
+                    child: CachedNetworkImage(
+                      imageUrl:
+                          'https://assets.coincap.io/assets/icons/${cryptoList[index].symbol.toLowerCase()}@2x.png',
+                      placeholder: (context, url) {
+                        return Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                              color: Color(0xff1C82AD),
+                              borderRadius: BorderRadius.circular(12)),
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(cryptoList[index].name,
+                          style: TextStyle(fontSize: 20, color: Colors.white)),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(cryptoList[index].symbol,
+                          style: TextStyle(fontSize: 16, color: Colors.white)),
+                    ],
+                  ),
+                  Spacer(),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text('\$${cryptoList[index].priceUSD.toStringAsFixed(2)}',
+                          style: TextStyle(fontSize: 20, color: Colors.white)),
+                      Text(cryptoList[index].changeInPersent.toStringAsFixed(2),
+                          style: TextStyle(fontSize: 20, color: Colors.white)),
+                    ],
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  getArraowWidget(cryptoList[index].changeInPersent),
+                  SizedBox(
+                    width: 8,
+                  ),
                 ],
               ),
-              Spacer(),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(cryptoList[index].priceUSD.toStringAsFixed(2),
-                      style: TextStyle(fontSize: 20)),
-                  Text(cryptoList[index].changeInPersent.toStringAsFixed(2),
-                      style: TextStyle(fontSize: 20)),
-                ],
-              ),
-              getArraowWidget(cryptoList[index].changeInPersent),
-              SizedBox(
-                width: 8,
-              ),
-            ],
-          ),
-        );
-      },
-    ));
+            );
+          },
+        ));
   }
 
   Widget getArraowWidget(double changeInPersent) {
     if (changeInPersent < 0) {
       return Icon(
-        Icons.arrow_downward,
+        Icons.trending_down,
         color: Colors.red,
       );
     } else {
       return Icon(
-        Icons.arrow_upward,
+        Icons.trending_up,
         color: Colors.green,
       );
     }
