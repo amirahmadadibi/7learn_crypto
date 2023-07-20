7b0da7c36667bb801b3620c2fc279807f47fe2a7
amirahmadadibi <amirahmadadibi@gmail.com>
2023-07-20 21:32:27 +0330

    competle redesign of the project

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:lifecycle/crypto.dart';
 import 'package:lifecycle/user.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -24,12 +25,61 @@ class _FirstScreenState extends State<FirstScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xff13005A),
+        backgroundColor: Color(0xffffffff),
         appBar: AppBar(
-          title: Text((isLoading) ? 'Loading...' : title),
-          centerTitle: true,
-          backgroundColor: Color(0xff03C988),
+          toolbarHeight: 200,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          flexibleSpace: Container(
+            height: 200,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                SizedBox(
+                    height: 70,
+                    width: 200,
+                    child: Image.asset('assets/images/logo.png')),
+                SizedBox(
+                  height: 30,
+                ),
+                Visibility(visible: isLoading, child: Text('Loading...')),
+                Visibility(
+                  visible: !isLoading,
+                  child: Container(
+                    height: 50,
+                    margin: EdgeInsets.symmetric(horizontal: 25),
+                    decoration: BoxDecoration(
+                        color: Color(0xffF5F5F5),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Row(children: [
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Image.asset('assets/images/search.png'),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                              hintText: 'Search', border: InputBorder.none),
+                        ),
+                      )
+                    ]),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
+
+        // // AppBar(
+        // //   title: Text((isLoading) ? 'Loading...' : title),
+        // //   centerTitle: true,
+        // //   backgroundColor: Color(0xff03C988),
+        // // ),
         body: RefreshIndicator(
           onRefresh: () async {
             getData();
@@ -38,80 +88,87 @@ class _FirstScreenState extends State<FirstScreen> {
             itemCount: cryptoList.length,
             itemBuilder: (context, index) {
               return Container(
-                height: 100,
-                margin: EdgeInsets.only(bottom: 10),
-                color: Color(0xff00337C),
-                child: Row(
+                height: 85,
+                margin: EdgeInsets.only(bottom: 10, left: 25, right: 25),
+                color: Color(0xffffffff),
+                child: Column(
                   children: [
-                    SizedBox(
-                      width: 8,
-                    ),
-                    Text(
-                      '${cryptoList[index].rank}',
-                      style: TextStyle(fontSize: 24, color: Colors.white),
-                    ),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    Container(
-                      height: 45,
-                      width: 45,
-                      child: CachedNetworkImage(
-                        imageUrl:
-                            'https://assets.coincap.io/assets/icons/${cryptoList[index].symbol.toLowerCase()}@2x.png',
-                        placeholder: (context, url) {
-                          return Container(
-                            height: 40,
-                            width: 40,
-                            decoration: BoxDecoration(
-                                color: Color(0xff1C82AD),
-                                borderRadius: BorderRadius.circular(12)),
-                          );
-                        },
-                      ),
-                    ),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    Row(
                       children: [
-                        Text(cryptoList[index].name,
-                            style:
-                                TextStyle(fontSize: 20, color: Colors.white)),
                         SizedBox(
-                          height: 8,
+                          width: 8,
                         ),
-                        Text(cryptoList[index].symbol,
-                            style:
-                                TextStyle(fontSize: 16, color: Colors.white)),
-                      ],
-                    ),
-                    Spacer(),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                            '\$${cryptoList[index].priceUSD.toStringAsFixed(2)}',
-                            style:
-                                TextStyle(fontSize: 20, color: Colors.white)),
-                        Text(
-                            cryptoList[index]
-                                .changeInPersent
-                                .toStringAsFixed(2),
-                            style:
-                                TextStyle(fontSize: 20, color: Colors.white)),
+                        Container(
+                          width: 45,
+                          child: CachedNetworkImage(
+                            imageUrl:
+                                'https://assets.coincap.io/assets/icons/${cryptoList[index].symbol.toLowerCase()}@2x.png',
+                            placeholder: (context, url) {
+                              return Container(
+                                height: 40,
+                                width: 40,
+                                decoration: BoxDecoration(
+                                    color: Color(0xff1C82AD),
+                                    borderRadius: BorderRadius.circular(12)),
+                              );
+                            },
+                          ),
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(cutDownCryptoName(cryptoList[index].name),
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.black)),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            Text(cryptoList[index].symbol,
+                                style: TextStyle(
+                                    fontSize: 16, color: Color(0xffAAB0BD))),
+                          ],
+                        ),
+                        Spacer(),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                                '\$${cryptoList[index].priceUSD.toStringAsFixed(2)}',
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.black)),
+                            Row(
+                              children: [
+                                getArraowWidget(
+                                    cryptoList[index].changeInPersent),
+                                Text(
+                                    "\$ " +
+                                        cryptoList[index]
+                                            .changeInPersent
+                                            .toStringAsFixed(2),
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        color:
+                                            (cryptoList[index].changeInPersent <
+                                                    0)
+                                                ? Colors.red
+                                                : Colors.green)),
+                              ],
+                            )
+                          ],
+                        )
                       ],
                     ),
                     SizedBox(
-                      width: 8,
+                      height: 5,
                     ),
-                    getArraowWidget(cryptoList[index].changeInPersent),
-                    SizedBox(
-                      width: 8,
-                    ),
+                    Divider(
+                      thickness: 0.5,
+                    )
                   ],
                 ),
               );
@@ -123,13 +180,15 @@ class _FirstScreenState extends State<FirstScreen> {
   Widget getArraowWidget(double changeInPersent) {
     if (changeInPersent < 0) {
       return Icon(
-        Icons.trending_down,
+        Icons.arrow_downward,
         color: Colors.red,
+        size: 15,
       );
     } else {
       return Icon(
-        Icons.trending_up,
+        Icons.arrow_upward,
         color: Colors.green,
+        size: 15,
       );
     }
   }
@@ -150,5 +209,13 @@ class _FirstScreenState extends State<FirstScreen> {
       isLoading = false;
       cryptoList = cryptoDataList;
     });
+  }
+
+  String cutDownCryptoName(String cryptoName) {
+    if (cryptoName.length > 15) {
+      var cuted = cryptoName.substring(0, 12);
+      return '$cuted...';
+    }
+    return cryptoName;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'pm'),
       home: FirstScreen(),
     );
   }
